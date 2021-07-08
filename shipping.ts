7cf6cf4cfac3a62912f753a0601691db53e15619
leolurch <40560457+leolurch@users.noreply.github.com>
2021-07-08 22:56:56 +0200

    Add files via upload

---
+++ shipping.ts
@@ -0,0 +1,119 @@
+// list of all eu countries with IS0-3166-1 codes and German names
+// https://www.auswaertiges-amt.de/de/service/fragenkatalog-node/02-ewr-eu/606444 - 07/08/2021
+
+const EU_COUNRIES = [
+    {
+        name: "Belgien",
+        code: "BE",
+    },
+    {
+        name: "Bulgarien",
+        code: "BG",
+    },
+    {
+        name: "Dänemark",
+        code: "DK",
+    },
+    {
+        name: "Deutschland",
+        code: "DE",
+    },
+    {
+        name: "Estland",
+        code: "EE",
+    },
+    {
+        name: "Finnland",
+        code: "FI",
+    },
+    {
+        name: "Frankreich",
+        code: "FR",
+    },
+    {
+        name: "Griechenland",
+        code: "GR",
+    },
+    {
+        name: "Irland",
+        code: "IE",
+    },
+    {
+        name: "Italien",
+        code: "IT",
+    },
+    {
+        name: "Kroatien",
+        code: "HR",
+    },
+    {
+        name: "Lettland",
+        code: "LV",
+    },
+    {
+        name: "Litauen",
+        code: "LT",
+    },
+    {
+        name: "Luxemburg",
+        code: "LU",
+    },
+    {
+        name: "Malta",
+        code: "MT",
+    },
+    {
+        name: "Niederlande",
+        code: "NL",
+    },
+    {
+        name: "Österreich",
+        code: "AT",
+    },
+    {
+        name: "Polen",
+        code: "PL",
+    },
+    {
+        name: "Portugal",
+        code: "PT",
+    },
+    {
+        name: "Niederlande",
+        code: "NL",
+    },
+    {
+        name: "Rumänien",
+        code: "RO",
+    },
+    {
+        name: "Schweden",
+        code: "SE",
+    },
+    {
+        name: "Slowakei",
+        code: "SK",
+    },
+    {
+        name: "Slowenien",
+        code: "SI",
+    },
+    {
+        name: "Spanien",
+        code: "ES",
+    },
+    {
+        name: "Tschechische Republik",
+        code: "CZ",
+    },
+    {
+        name: "Ungarn",
+        code: "HU",
+    },
+    {
+        name: "Zypern",
+        code: "CY",
+    }
+];
+
+export default EU_COUNRIES;
